dd7ddb1e1cea94d08e0aa9cc172ebcb21ab7a6dd
Ayala <a9791606@gmail.com>
2026-03-10 18:34:09 +0200

    update project

--- Ice cream project/Models/IceCream.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IceCreamNamespace.Models
-{
-    public class IceCream
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool isGloutenFree { get; set; }
-    }
-}
