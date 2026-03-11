41d3c1b5f7f3f75330ba1703e175f4ff0d97dc64
Ayala <a9791606@gmail.com>
2026-03-11 02:08:22 +0200

    Deleting unnecessary things

--- last ice project/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace IceCreamNamespace.Models;
-  public class User
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-       public string Password { get; set; }
-    }
